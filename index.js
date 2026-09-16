53f6e094e97ebc3f8090698a91cf0b88bdfa678b
dom1235 <kupeczikszabolcs-50111@taszi.hu>
2026-09-16 14:41:05 +0200

    Done

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const app = express()
 const PORT = 3030;
 
 const { getkonyvek,
-        getKonyvById
+        getKonyvById,
+        getKonyvekByar,
+        getKonyvDarab,
+        getKonyvByCim,
+        getKonyvBySzerzo,
+        getKonyvKategoria,
+        getKonyvekArfelett,
+        getKonyvekByOldalszam
+        
  } = require("./services/konyvekservice");
 //ez kell ahhoz hogy request bodyt tudjak kuldeni
 app.use(express.json());
@@ -14,7 +22,16 @@ app.get("/", (req, res) =>{
 
 // routes
 app.get("/konyvek", getkonyvek)
+app.get("/konyvek/db", getKonyvDarab)
 app.get("/konyvek/:id", getKonyvById)
+app.get("/konyvek/ar/:ar", getKonyvekByar)
+app.get("/konyvek/cim/:cim", getKonyvByCim)
+app.get("/konyvek/szerzo/:szerzo",getKonyvBySzerzo);
+app.get("/konyvek/kategoria/:kategoria", getKonyvKategoria)
+app.get("/konyvek/arfelett/:arfelett",getKonyvekArfelett)
+app.get("/konyvek/oldalszam/:olszaldszam",getKonyvekByOldalszam)
+
+
 
 app.listen(PORT, () => {
     console.log("Szerver elinduult a " + PORT + "-on")
--- services/konyvekservice.js
+++ services/konyvekservice.js
@@ -11,7 +11,66 @@ const getKonyvById = (req,res) => {
     res.json(konyv);
 }
 
+const getKonyvekByar = (req, res) => {
+    let paramAr = req.params.ar;
+    let getKonyvekByar = konyvek.filter(konyv => konyv.price < paramAr)
+    res.json(getKonyvekByar)
+}
+
+const getKonyvDarab = (req,res) => {
+    res.json({darab: konyvek.length})
+}
+
+const getKonyvByCim = (req, res) => {
+    let paramCim = req.params.cim.toLowerCase().trim();
+    const konyv = konyvek.filter(konyv => {
+        let kisbetus = konyv.title.toLowerCase().replaceAll(' ', '').trim();
+        if (kisbetus == paramCim)
+            return konyv;
+    });
+    res.json(konyv);
+};
+
+const getKonyvBySzerzo = (req, res) => {
+    let szerzok = req.params.szerzo.toLowerCase().replaceAll(' ', '').trim();
+    const konyv = konyvek.filter(konyv => {
+        let kisbetus = konyv.author.toLowerCase().replaceAll(' ', '').trim();
+        if (kisbetus == szerzok)
+            return konyv;
+    });
+    res.json(konyv);
+};
+
+const getKonyvKategoria = (req, res) => {
+    let kategoria = req.params.kategoria.toLowerCase().replaceAll(' ', '');
+    const konyv = konyvek.filter(konyv => {
+        let kisbetus = konyv.category.toLowerCase().replaceAll(' ', '');
+        if (kisbetus == kategoria)
+            return konyv;
+    });
+    res.json(konyv);
+};
+
+const getKonyvekArfelett = (req, res) => {
+    let paramAr = req.params.arfelett;
+    let getKonyvekArfelett = konyvek.filter(konyv => konyv.price > paramAr)
+    res.json(getKonyvekArfelett)
+}
+
+const getKonyvekByOldalszam = (req, res) => {
+    const paramOldalszam = Number(req.params.paramOldalszam)
+    const konyvekByOldalszam = konyvek.filter(konyv => konyv.pages < paramOldalszam)
+    res.json(getKonyvekByOldalszam)
+}
+
 module.exports = {
     getkonyvek,
-    getKonyvById
+    getKonyvById,
+    getKonyvekByar,
+    getKonyvDarab,
+    getKonyvByCim,
+    getKonyvBySzerzo,
+    getKonyvKategoria,
+    getKonyvekArfelett,
+    getKonyvekByOldalszam
 }
